c9eb194f3fe129a0f51d8b3b7303ea993ce2c72e
catierosepate <cpate@lsoc.org>
2014-03-21 13:15:48 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poliproj::Application.config.secret_key_base = '70dda7d91ff9aa3931efccda90d9ab0c53cb4ea395dd3afe9cbc9894cab18488636b6a6dee8a99dea327805bedf613f334bd7a28571491ca5d25856bd95077f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Poliproj::Application.config.session_store :cookie_store, key: '_poliproj_session'
